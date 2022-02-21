35229db6abceb0e05edb78544b3f6526be8b99d9
Florian Stascheck <florian@adventures.tech>
2022-02-21 22:17:55 +0000

    ExperienceFilterInput: allow to sync non-own experiences

--- src/GraphQL/DTOs/ExperienceFilterInput.php
+++ src/GraphQL/DTOs/ExperienceFilterInput.php
@@ -5,11 +5,11 @@ namespace Adventures\LaravelBokun\GraphQL\DTOs;
 class ExperienceFilterInput extends DTO
 {
     public function __construct(
-        public int $supplierId,
-        public string $searchTerm = '',
+        public ?int $supplierId = null,
+        public ?string $searchTerm = '',
         public ?string $lastModifiedAfter = null,
         public ?string $vendorRole = null,
-        public bool $activated = true
+        public ?bool $activated = true
     ) {
     }
 }
