d9d6ec6a5f52af3a65207682df424c01852bf9db
Florian Stascheck <florian@adventures.tech>
2022-03-24 11:29:08 +0000

    experience booking: make fields optional

--- src/GraphQL/DTOs/ExperienceBooking.php
+++ src/GraphQL/DTOs/ExperienceBooking.php
@@ -18,13 +18,13 @@ class ExperienceBooking extends DTO
 
     public function __construct(
         public int $id,
-        public string $reference,
-        public Experience $experience,
         public int $zonedStartDateAndTime,
-        public int $totalPassengerCount,
-        public int $totalSeatsOccupied,
+        public ?Experience $experience = null,
         #[ArrayOf(PassengerBooking::class)]
-        public array $passengers,
+        public array $passengers = [],
+        public ?int $totalPassengerCount = null,
+        public ?int $totalSeatsOccupied = null,
+        public ?string $reference = null,
         public ?ExperienceStartTime $startTime = null,
         public ?BookingStatus $bookingStatus = null,
     ) {
