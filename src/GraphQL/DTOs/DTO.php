ac5195448161e78ca72c80b325230d5c3b5619cf
Florian Stascheck <florian@adventures.tech>
2022-05-18 11:24:41 +0000

    Better error message on DTO sync error

--- src/GraphQL/DTOs/DTO.php
+++ src/GraphQL/DTOs/DTO.php
@@ -94,7 +94,7 @@ abstract class DTO
             return [$name => $ids];
         }
 
-        throw new RuntimeException("a field that's an ID has to be string or array in API return.");
+        throw new RuntimeException("a field that's an ID has to be string or array in API return. Actual value: " . json_encode($value) . " - Class: " . get_called_class());
     }
 
     /**
