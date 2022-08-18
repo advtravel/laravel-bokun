26ae5db543f1fc2ddc9200e623363be5093bad3b
levu42 <levu42@users.noreply.github.com>
2022-08-18 14:24:57 +0000

    Fix styling

--- src/AppStore/BokunAppStoreOperator.php
+++ src/AppStore/BokunAppStoreOperator.php
@@ -5,6 +5,8 @@ namespace Adventures\LaravelBokun\AppStore;
 interface BokunAppStoreOperator
 {
     public function isFullyOAuthConnected(): bool;
+
     public function updateWithOperatorDetails(OperatorDetails $info): void;
+
     public function getAccessToken(): string;
 }
