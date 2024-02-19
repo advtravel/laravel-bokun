3cfe7fa513b39ed4b8a9f4f72906630f773aa10e
Florian Stascheck <florian@adventures.tech>
2024-02-19 11:46:31 +0000

    typo

--- src/GraphQL/Types/BookingPaymentStatus.php
+++ src/GraphQL/Types/BookingPaymentStatus.php
@@ -6,7 +6,7 @@ enum BookingPaymentStatus: string
 {
     case Deposit = 'DEPOSIT';
     case Free = 'FREE';
-    case Invoices = 'INVOICED';
+    case Invoiced = 'INVOICED';
     case NotPaid = 'NOT_PAID';
     case PaidInFull = 'PAID_IN_FULL';
 }
